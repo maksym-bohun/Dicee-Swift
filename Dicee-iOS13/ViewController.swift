335ae74c276bee5c270527c7b139841a98e30b47
Maksym-Bohun <maks.boggun@gmail.com>
2023-09-17 22:27:25 +0300

    Initial Commit

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // IBOutlet allows me to reference the elements
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        var images = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix"),
+        ]
+        diceImageView1.image = images[Int.random(in: 0..<6)]
+        diceImageView2.image = images[Int.random(in: 0..<6)]
+    }
+    
+}
+
